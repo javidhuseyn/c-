755d7cef755c5f06985ba76ed1d1d28e2db0322f
javidhuseyn <49587798+javidhuseyn@users.noreply.github.com>
2019-07-22 23:44:10 +0400

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace formBir
+{
+    public partial class Form1 : Form
+    {
+        List<string> grupList;
+        public Form1()
+        {
+            grupList = new List<string>();
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            string listbox = listBox.Text;
+            if (listbox.Length != 0)
+            {
+                grupList.Add(listbox);
+                grupBox.Items.Clear();
+                foreach (var grp in grupList) 
+                {
+                    grupBox.Items.Add(grp);
+                }
+            }
+            else
+            {
+                label5.Visible = true;
+            }
+
+
+        }
+    }
+}
